5bbd21260ced43ea4126f24d2a96e61e7675b7f5
Kyle Evangelisto <kyleevangelisto@gmail.com>
2022-04-13 09:16:25 -0400

    Part 3 Original Submission

--- include/console.h
+++ include/console.h
@@ -2,9 +2,37 @@ static char* const VGA_MEMORY = (char*) 0xb8000;
 static const int VGA_WIDTH = 80;
 static const int VGA_HEIGHT = 25;
 static const int SCREEN_BYTES_WIDTH = VGA_WIDTH * 2;
+static const int TOTAL_SCREEN_DISPLAY = VGA_HEIGHT * VGA_WIDTH;
 static int Terminal_Position = 0;
+
+enum Color {
+  BLACK = 0,
+  BLUE,
+  GREEN,
+  CYAN,
+  RED,
+  PURPLE,
+  BROWN,
+  GRAY,
+  DARK_GRAY,
+  LIGHT_BLUE,
+  LIGHT_GREEN,
+  LIGHT_CYAN,
+  LIGHT_RED,
+  LIGHT_PURPLE,
+  YELLOW,
+  WHITE
+};
+
+// Method headers
 
 void print_character(char);
 void print_string(char*);
 void print_line(char*);
+
+void set_terminal_font_color(enum Color);
+void print_character_with_color(char, enum Color);
+void print_string_with_color(char*, enum Color);
+void print_line_with_color(char*, enum Color);
+void shift_terminal_up();
 
--- shell/console.c
+++ shell/console.c
@@ -1,9 +1,21 @@
 #include "../include/console.h"
 
+int terminal_font_color = GRAY; //default to gray
+
+void set_terminal_font_color(enum Color color) {
+  terminal_font_color = color;	
+}
+
 void print_character(char c) {
   VGA_MEMORY[Terminal_Position] = c; 
-  VGA_MEMORY[Terminal_Position + 1] = 0x07; //Default to Gray color
+  VGA_MEMORY[Terminal_Position + 1] = terminal_font_color;
   Terminal_Position = Terminal_Position + 2; //increment curernt cursor position to next address  
+}
+
+void print_character_with_color(char c, enum Color color) {
+  VGA_MEMORY[Terminal_Position] = c;
+  VGA_MEMORY[Terminal_Position + 1] = color;
+  Terminal_Position = Terminal_Position + 2;
 }
 
 void print_string(char* str) {
@@ -16,8 +28,31 @@ void print_string(char* str) {
 
 }
 
+void print_string_with_color(char* str, enum Color color) {
+
+  unsigned int i = 0;
+  while(str[i] != '\0') {
+    print_character_with_color(str[i], color);
+    i++;
+  }  
+
+}
+
 void print_line(char* str) {
   print_string(str);
   Terminal_Position = (Terminal_Position - (Terminal_Position % SCREEN_BYTES_WIDTH)); 
   Terminal_Position += SCREEN_BYTES_WIDTH; 
+}
+
+void print_line_with_color(char* str, enum Color color) {
+  print_string_with_color(str, color);
+  Terminal_Position = (Terminal_Position - (Terminal_Position % SCREEN_BYTES_WIDTH));
+  Terminal_Position += SCREEN_BYTES_WIDTH;	
+}
+
+// Shift all lines up the terminal screen.
+void shift_terminal_up() {
+  for (int i = 0; i < TOTAL_SCREEN_DISPLAY; i++) {
+    VGA_MEMORY[i] = VGA_MEMORY[i + (2 * VGA_WIDTH)];
+  }  
 }
